967ea6cae312574d9d19be090e82ebbcf9d29107
Zack <6475989+dot-iso@users.noreply.github.com>
2019-04-10 11:34:59 -0700

    Add files via upload
    Initial Commit

---
+++ airportwx.py
@@ -0,0 +1,23 @@
+# Import Libraries
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+
+def main(airport):
+	# Specify URL
+	airport_page = 'http://www.airnav.com/airport/' + airport
+
+	# Query the webpage and return the html
+	http = urllib3.PoolManager()
+	page = http.request('GET', airport_page)
+
+	# Parse
+	soup = BeautifulSoup(page.data, 'html.parser')
+
+	html_string = 'body > table:nth-child(8)'
+	find = soup.find(string="METAR").find_next('tr').find_next('table').find_next('tr').text
+
+	print(find)
+
+if __name__ == "__main__":
+	main(sys.argv[1])
